9a5c4cd859b3bb388467e423ca4d4b33a863f4f2
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-11-29 07:51:19 +0600

    make premium btn req to admin

--- src/component/Layout/Dashboard.jsx
+++ src/component/Layout/Dashboard.jsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { AuthContext } from '../Providers/AuthProvider';
+import useAxiosSecure from '../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
+import useUsers from '../hooks/useUsers';
 
 const Dashboard = () => {
+
+    const { user } = useContext(AuthContext);
+    const axiosSecure = useAxiosSecure();
+    const [users,] = useUsers();
+    // console.log(users);
+    // console.log(user?.email);
+
+    const currentUser = users.find(userData => userData?.email === user?.email);
+
+    const userData = {
+        name: currentUser?.name,
+        email: currentUser?.email, 
+        bioId: currentUser?.bioId
+    };
+    // console.log(userData);
+
+
+
+    const makePremium = () => {
+        console.log(`Making user with email premium`);
+
+        Swal.fire({
+            title: "Send Premium Request To Admin?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Send Request !"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.post(`/userToPremium`, userData)
+                    .then(res => {
+                        if (res.data.insertedId) {
+
+                            Swal.fire({
+                                title: "Request Sent!!",
+                                text: "Waiting for Admin Approval.",
+                                icon: "success"
+                            });
+                        }
+                        else {
+                            console.log('user not updating to Premium..!');
+                            Swal.fire({
+                                icon: "error",
+                                title: "You Already Sent A Request!",
+                                text: "Wait For Admin Approval",
+                              });
+                        }
+                    })
+
+            }
+        });
+
+    };
+
+
     return (
         <div className="flex">
             <div className="w-64 p-8 min-h-full bg-purple-200">
@@ -10,7 +72,7 @@ const Dashboard = () => {
                         <NavLink to={"/dashboard/adminDashboard"} className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">Admin Dashboard</NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/dashboard/manageUsers"}  className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">Manage Users</NavLink>
+                        <NavLink to={"/dashboard/manageUsers"} className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">Manage Users</NavLink>
                     </li>
                     <li>
                         <NavLink to={"/dashboard/myContactRequest"} className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">My Contact Request</NavLink>
@@ -21,11 +83,17 @@ const Dashboard = () => {
                     <li>
                         <hr className='my-8' />
                     </li>
+                    <button onClick={() => makePremium()} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-10 rounded ">
+                        Make Premium
+                    </button>
+                    <li>
+                        <but to={"/dashboard/editInfo"} className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">Edit Biodata</but>
+                    </li>
                     <li>
                         <NavLink to={"/dashboard/editInfo"} className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">Edit Biodata</NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/dashboard/userInfo"}  className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">View Biodata</NavLink>
+                        <NavLink to={"/dashboard/userInfo"} className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">View Biodata</NavLink>
                     </li>
                     <li>
                         <NavLink to={"/dashboard/myContactRequest"} className="block py-2 bg-slate-100 px-4 rounded-md hover:bg-purple-300">My Contact Request</NavLink>
@@ -36,8 +104,8 @@ const Dashboard = () => {
                     <li>
                         <button className="block py-2  px-4 rounded-md bg-purple-600 text-white hover:bg-purple-700">Logout</button>
                     </li>
-                    
-                    
+
+
                 </ul>
             </div>
             <div className="flex-1 p-8">
