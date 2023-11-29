544757e2dfcfa122e3aa1397bb745bb5848c5b95
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-11-29 06:52:23 +0600

    make admin, make premium

--- src/component/Dashboard/Admin/ManageUsers.jsx
+++ src/component/Dashboard/Admin/ManageUsers.jsx
@@ -1,32 +1,97 @@
+import Swal from "sweetalert2";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import useUsers from "../../hooks/useUsers";
 
 
 
 const ManageUsers = () => {
-    const [users,]  = useUsers();
+    const [users, refetch] = useUsers();
     const axiosSecure = useAxiosSecure();
     console.log(users);
 
+    const adminView = () => {
+        Swal.fire({
+            title: "Already an Admin",
+            icon: "warning"
+        });
+    }
+    const premiumView = () => {
+        Swal.fire({
+            title: "Already Premium User",
+            icon: "warning"
+        });
+    }
 
     const makeAdmin = (userId) => {
         console.log(`Making user with ID ${userId} an admin`);
-        // Perform action to make the user an admin using the userId
-        axiosSecure.patch(`/usersToAdmin/${userId}`)
-        .then(res=>{
-            if(res.data.modifiedCount){
 
-                console.log('user is admin now');
+        Swal.fire({
+            title: "Making an Admin?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, make admin!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.patch(`/userToAdmin/${userId}`)
+                    .then(res => {
+                        if (res.data.modifiedCount) {
+                            refetch();
+
+                            console.log('user is admin now');
+                            Swal.fire({
+                                title: "The User is Admin Now!",
+                                text: "New Admin Arrived!!.",
+                                icon: "success"
+                            });
+                        }
+                        else {
+                            console.log('user not updating to admin..!');
+                        }
+                    })
+
             }
-            else{
-                console.log('user not updating to admin..!');
-            }
-        })
+        });
+
+
     };
 
     const makePremium = (userId) => {
         console.log(`Making user with ID ${userId} premium`);
-        // Perform action to make the user premium using the userId
+
+        Swal.fire({
+            title: "Making Premium User?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, make premium!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.patch(`/userToPremium/${userId}`)
+                    .then(res => {
+                        if (res.data.modifiedCount) {
+                            refetch();
+
+                            console.log('user is Premium now');
+                            Swal.fire({
+                                title: "The User is Premium Now!",
+                                text: "New Premium User Arrived!!.",
+                                icon: "success"
+                            });
+                        }
+                        else {
+                            console.log('user not updating to Premium..!');
+                        }
+                    })
+
+            }
+        });
     };
 
 
@@ -40,16 +105,31 @@ const ManageUsers = () => {
             <div className="lg:hidden">
                 {users?.map((item, i) => (
                     <div key={i} className="bg-white border rounded-lg shadow-md p-4 mb-4">
-                        {i+1}.
+                        {i + 1}.
                         <p className="border-b border-gray-200 pb-2"><span className="font-semibold">Name:</span> {item?.name}</p>
                         <p className="border-b border-gray-200 pb-2"><span className="font-semibold">Email:</span> {item?.email}</p>
 
-                        <button onClick={() => makeAdmin(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
-                            make admin
-                        </button>
-                        <button onClick={() => makePremium(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
-                            make premium
-                        </button>
+                        {
+                            item?.role === 'admin' ?
+                                <button onClick={() => adminView()} className="mt-2 bg-pink-700 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                    Admin
+                                </button>
+                                :
+                                <button onClick={() => makeAdmin(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                    Make Admin
+                                </button>
+                        }
+                        {
+                            item?.userType === 'premium' ?
+                                <button onClick={() => premiumView()} className="mt-2 bg-pink-700 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                    Premium User
+                                </button>
+                                :
+                                <button onClick={() => makePremium(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                    Make Premium
+                                </button>
+                        }
+
 
                         {/* Additional fields as needed */}
                     </div>
@@ -73,13 +153,13 @@ const ManageUsers = () => {
                                                 Email
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Action - user api
+                                                Action
                                             </th>
                                             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Action user api
+                                                Action
                                             </th>
-                                            
-                                            
+
+
 
                                         </tr>
                                     </thead>
@@ -87,21 +167,36 @@ const ManageUsers = () => {
                                         {users?.map((item, i) => (
                                             <tr key={i} className="hover:bg-gray-100">
                                                 <td className="px-6 py-4 whitespace-nowrap">
-                                                    <div className="text-sm font-medium text-gray-900">{i+1}. {item?.name}</div>
+                                                    <div className="text-sm font-medium text-gray-900">{i + 1}. {item?.name}</div>
                                                 </td>
                                                 <td className="px-6 py-4 whitespace-nowrap">
                                                     <div className="text-sm font-medium text-gray-900">{item?.email}</div>
                                                 </td>
                                                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                <button onClick={() => makeAdmin(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
-                                                    Make Admin
-                                                </button>
-                                            </th>
+                                                    {
+                                                        item?.role === 'admin' ?
+                                                            <button onClick={() => adminView()} className="mt-2 bg-pink-700 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                                                Admin
+                                                            </button>
+                                                            :
+                                                            <button onClick={() => makeAdmin(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                                                Make Admin
+                                                            </button>
+                                                    }
+                                                </th>
                                                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                <button onClick={() => makePremium(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
-                                                    Make Premium
-                                                </button>
-                                            </th>
+                                                    {
+                                                        item?.userType === 'premium' ?
+                                                            <button onClick={() => premiumView()} className="mt-2 bg-pink-700 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                                                Premium User
+                                                            </button>
+                                                            :
+                                                            <button onClick={() => makeAdmin(item?._id)} className="mt-2 bg-pink-500 hover:bg-pink-600 text-white py-1 px-3 rounded mx-1">
+                                                                Make Admin
+                                                            </button>
+                                                    }
+                                                </th>
+
 
                                             </tr>
                                         ))}
